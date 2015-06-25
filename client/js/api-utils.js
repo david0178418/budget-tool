97a8644ca7e06b099a1ceaeae72670f58f3ded70
David Granado <davidjgranado@gmail.com>
2015-06-24 23:13:45 -0500

    Remove code from charts project

--- client/js/api-utils.js
+++ client/js/api-utils.js
@@ -1,30 +1,5 @@
 'use strict';
-import AppConstants from 'app-constants';
 import Api from 'api';
 
-var utils = {
-	getPlatforms: function() {
-		return Api.getPlatforms();
-	},
-
-	getPlatformsTopTen: function(date) {
-		return Api.getPlatformsTopTen(date);
-	},
-
-	getSeriesData: function(params) {
-		var xhr;
-		var apiCall;
-
-		if(params.type === AppConstants.GRAPH_CATEGORY_PLATFORMS) {
-			apiCall = Api.getPlatformsData;
-		} else {
-			apiCall = Api.getProjectData;
-		}
-
-		xhr = apiCall(params.zoom, params.ids);
-
-		return xhr;
-	},
+export default {
 };
-
-export default utils;
