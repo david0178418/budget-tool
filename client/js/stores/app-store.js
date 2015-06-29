7d1eac0e11420e2222dc2cab023e464557213544
David Granado <davidjgranado@gmail.com>
2015-06-29 17:47:43 -0500

    Remove old code

--- client/js/stores/app-store.js
+++ client/js/stores/app-store.js
@@ -3,7 +3,6 @@ import appActions from 'actions/app-actions';
 import AppConstants from 'app-constants';
 import extend from 'lodash/object/extend';
 import flux from 'flux';
-import serverActions from 'actions/server-actions';
 
 class AppStore {
 	constructor() {
