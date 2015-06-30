00f1c6ddd98831d9dc578487c9a97fb897dccc8e
David Granado <davidjgranado@gmail.com>
2015-06-30 00:00:31 -0500

    Move app constants file to root

--- app-constants.js
+++ app-constants.js
@@ -1,4 +1,12 @@
 var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
+	BUDGET_TYPE_EXPENSE: null,
+	BUDGET_TYPE_INCOME: null,
+	INTERVAL_ONE_TIME:null,
+	INTERVAL_DAILY: null,
+	INTERVAL_WEEKLY: null,
+	INTERVAL_BI_WEEKLY: null,
+	INTERVAL_MONTHLY: null,
+	INTERVAL_CUSTOM: null,
 });
--- client/js/app-constants.js
+++
@@ -1,5 +0,0 @@
-var keyMirror = require('react/lib/keyMirror');
-
-// TODO Break these up?
-module.exports = keyMirror({
-});
