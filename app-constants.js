3e725bb42ed9a2e2db41d456a6b7a6e81433c696
David Granado <davidjgranado@gmail.com>
2015-07-29 22:49:57 -0500

    Additional ideas on layout of app

--- app-constants.js
+++ app-constants.js
@@ -1,9 +1,10 @@
+/*global require, module*/
 var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
 	BUDGET_TYPE_EXPENSE: null,
 	BUDGET_TYPE_INCOME: null,
-	INTERVAL_ONE_TIME:null,
+	INTERVAL_ONE_TIME: null,
 	INTERVAL_DAILY: null,
 	INTERVAL_WEEKLY: null,
 	INTERVAL_BI_WEEKLY: null,
--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,48 +1,33 @@
 import AltContainer from 'alt/AltContainer';
 import classNames from 'classnames';
 import flux from 'flux';
+import keyMirror from 'react/lib/keyMirror';
 import React from 'react/addons';
 
 import './app.scss';
 
+var TABS = keyMirror({
+	CALENDAR: null,
+	DAY: null,
+	GRAPH: null,
+	ITEMS: null,
+});
+
+export default
 class App extends React.Component {
+	static get displayName() {
+		return 'App';
+	}
+
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			selectedTab: 'DAY',
+			selectedTab: TABS.DAY,
 		};
 	}
 
-	onTabSelect(tab) {
-		this.setState({
-			selectedTab: tab,
-		});
-	}
-
 	render() {
-		var dayTabClasses = classNames({
-			active: this.state.selectedTab === 'DAY',
-		});
-		var graphTabClasses = classNames({
-			active: this.state.selectedTab === 'GRAPH',
-		});
-		var calendarTabClasses = classNames({
-			active: this.state.selectedTab === 'CALENDAR',
-		});
-		var dayTabPanelClasses = classNames({
-			active: this.state.selectedTab === 'DAY',
-			'tab-pane': true,
-		});
-		var graphTabPanelClasses = classNames({
-			active: this.state.selectedTab === 'GRAPH',
-			'tab-pane': true,
-		});
-		var calendarTabPanelClasses = classNames({
-			active: this.state.selectedTab === 'CALENDAR',
-			'tab-pane': true,
-		});
-
 		return (
 			<AltContainer flux={flux}>
 				<div>
@@ -53,39 +38,53 @@ class App extends React.Component {
 					</div>
 					<div className="budget-detail">
 						<ul className="nav nav-tabs">
-							<li className={dayTabClasses}>
-								<a href="#" onClick={this.onTabSelect.bind(this, 'DAY')}>Day</a>
+							<li className={classNames({
+								active: this.state.selectedTab === TABS.DAY,
+							})}>
+								<a href="#" onClick={this.onTabSelect.bind(this, TABS.DAY)}>Day</a>
 							</li>
-							<li className={graphTabClasses}>
-								<a href="#" onClick={this.onTabSelect.bind(this, 'GRAPH')}>Graph</a>
+							<li className={classNames({
+								active: this.state.selectedTab === TABS.GRAPH,
+							})}>
+								<a href="#" onClick={this.onTabSelect.bind(this, TABS.GRAPH)}>Graph</a>
 							</li>
-							<li className={calendarTabClasses}>
+							<li className={classNames({
+								active: this.state.selectedTab === TABS.CALENDAR,
+							})}>
 								{/*
 									Does this make sense here? If the view is to
 									a fixed date renge, calendar view would be
 									an odd-ball out.  Seems like it's a level up maybe?
 								*/}
-								<a href="#" onClick={this.onTabSelect.bind(null, 'CALENDAR')}>Calendar(?)</a>
+								<a href="#" onClick={this.onTabSelect.bind(this, TABS.CALENDAR)}>Calendar(?)</a>
+							</li>
+							<li className={classNames({
+								active: this.state.selectedTab === TABS.CALENDAR,
+							})}>
+								<a href="#" onClick={this.onTabSelect.bind(this, TABS.ITEMS)}>Budget Items</a>
 							</li>
 						</ul>
-						<div className="tab-content">
-							<div className={dayTabPanelClasses}>
+						<div className="budget-detail-tabs tab-content">
+							<div className={classNames({
+								active: this.state.selectedTab === TABS.DAY,
+								'tab-pane': true,
+							})}>
 								<ul className="budget-items list-group">
 									<div className="list-group-item">
+										<button className="detail-toggle add-budget-item btn btn-default btn-xs" type="button">
+											<span className="glyphicon glyphicon-menu-down"></span>
+										</button>
 										<div className="budget-item">
 											$123 - Item 1 - Constant
-											<span className="buget-item-controls btn-group">
-												<button className="btn btn-default btn-xs">
-													<span className="glyphicon glyphicon-pencil"></span>
-												</button>
-												<button className="btn btn-default btn-xs">
-													<span className="glyphicon glyphicon-remove"></span>
-												</button>
-											</span>
-											<button className="detail-toggle add-budget-item btn btn-default btn-xs" type="button">
-												<span className="glyphicon glyphicon-menu-down"></span>
-											</button>
 										</div>
+										<span className="buget-item-controls btn-group">
+											<button className="btn btn-default btn-xs">
+												<span className="glyphicon glyphicon-pencil"></span>
+											</button>
+											<button className="btn btn-default btn-xs">
+												<span className="glyphicon glyphicon-remove"></span>
+											</button>
+										</span>
 									</div>
 									<div className="list-group-item">
 										<div className="budget-item expanded">
@@ -93,6 +92,9 @@ class App extends React.Component {
 												<span className="glyphicon glyphicon-menu-up"></span>
 											</button>
 											$123 - Item 2 - Variable
+											<div className="budget-item-detail">
+												Started: 1/1/2015
+											</div>
 											<span className="buget-item-controls btn-group">
 												<button className="btn btn-default btn-xs">
 													<span className="glyphicon glyphicon-pencil"></span>
@@ -101,14 +103,14 @@ class App extends React.Component {
 													<span className="glyphicon glyphicon-remove"></span>
 												</button>
 											</span>
-											<div className="budget-item-detail">
-												Started: 1/1/2015
-											</div>
 										</div>
 									</div>
 								</ul>
 							</div>
-							<div className={graphTabPanelClasses}>
+							<div className={classNames({
+								active: this.state.selectedTab === TABS.GRAPH,
+								'tab-pane': true,
+							})}>
 								<div className="toolbar">
 									<div className="date-range-control">
 										<select className="preset-interval">
@@ -148,14 +150,28 @@ class App extends React.Component {
 									</span>
 								</div>
 							</div>
+							<div className={classNames({
+								active: this.state.selectedTab === TABS.CALENDAR,
+								'tab-pane': true,
+							})}>
+								Calendar
+							</div>
+							<div className={classNames({
+								active: this.state.selectedTab === TABS.ITEMS,
+								'tab-pane': true,
+							})}>
+								Full items list
+							</div>
 						</div>
 					</div>
 				</div>
 			</AltContainer>
 		);
 	}
+
+	onTabSelect(tab) {
+		this.setState({
+			selectedTab: tab,
+		});
+	}
 }
-
-App.displayName = 'App';
-
-export default App;
